891b505f6dd45a93f0bebe40f4c3109f1b7d5b1c
orakalove <orakaloveth95@gmail.com>
2023-06-02 08:58:42 -0700

    link 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - function that frees a list_t list
+ * @head: pointer to the structure
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	temp = head;
+	while (temp != NULL)
+	{
+		free(temp->str);
+		free(temp);
+		temp = temp->next;
+	}
+}
