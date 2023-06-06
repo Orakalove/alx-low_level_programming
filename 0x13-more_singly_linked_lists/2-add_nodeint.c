e4cce49c52cbf7b6cc78e6a895e0c4eb38c1e827
orakalove <orakaloveth95@gmail.com>
2023-06-06 03:03:40 -0700

    list 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * add_nodeint - function that adds a new node at the beginning of a list
+ * @head: head
+ * @n: input
+ * Return: address of the new element
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
